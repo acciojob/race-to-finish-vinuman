2d7fa9f7b0442ea559f6621ae58a21143b5f8e0e
vinu cyril <38722628+vinuman@users.noreply.github.com>
2023-04-07 17:04:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+promises = [
+  new Promise(resolve => setTimeout(() => resolve(1), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(2), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(3), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(4), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(5), Math.floor(Math.random() * 5000) + 1000))
+];
+
+Promise.any(promises)
+  .then(result => {
+    // Print the result to the output div
+    output.textContent = `The first promise that resolved returned ${result}.`;
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error(error);
+  });
